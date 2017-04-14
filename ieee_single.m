c16e42bdd87e202e1e044f2db9837fb23be60afa
Fichberg <rfichberg@gmail.com>
2017-04-14 17:23:14 -0300

    Refactored to enable really big numbers (at the price of some performance). Now converts the number STRING to the binary string

--- ieee_single.m
+++ ieee_single.m
@@ -11,14 +11,14 @@ function main()
 
   while true
     a = scan_keyboard("Input value for A:\n>> ");
-    [a_value, a_sign] = input_to_number(a);
-    a_ieee_string = number_to_ieee_single(a_value, a_sign);
-    printf("A = "); print_word(a_ieee_string);
+    [a_value_bstring, a_sign_bstring] = input_to_number(a);
+    a_ieee_bstring = number_to_ieee_single(a_value_bstring, a_sign_bstring);
+    printf("A = "); print_word(a_ieee_bstring);
 
     b = scan_keyboard("Input value for B:\n>> ");
-    [b_value, b_sign] = input_to_number(b);
-    b_ieee_string = number_to_ieee_single(b_value, b_sign);
-    printf("B = "); print_word(b_ieee_string);
+    [b_value_bstring, b_sign_bstring] = input_to_number(b);
+    b_ieee_bstring = number_to_ieee_single(b_value_bstring, b_sign_bstring);
+    printf("B = "); print_word(b_ieee_bstring);
 
     op = scan_keyboard("Select an operation (+ or -):\n>> ");
     #ieee_string = operate(op, a_value, b_value);
@@ -27,26 +27,23 @@ function main()
 endfunction
 
 # Converts number to IEEE Single Format
-function ieee_string = number_to_ieee_single(number, sign)
-  fractional = abs(number) - floor(abs(number));
-  decimal = abs(number) - fractional;
+function ieee_string = number_to_ieee_single(number_bstring, sign_bstring)
+  occ = strchr(number_bstring, '.');
+  # Integer number
+  if length(occ) == 0
+    dec_bstring = number_bstring;
+    frac_bstring = "";
+  # Floating point number.
+  else
+    dec_bstring = substr(number_bstring, 1, occ - 1);
+    frac_bstring = substr(number_bstring, occ + 1);
+  endif
 
-  ieee_string = build_iee_single_representation(decimal, fractional, sign);
+  ieee_string = build_iee_single_representation(dec_bstring, frac_bstring, sign_bstring);
 endfunction
 
 # Constructs string representing the number in IEEE Single Format
-function ieee_string = build_iee_single_representation(decimal, fractional, sign_bstring)
-  # Decimal part
-  dec_bstring = dec2bin(decimal);
-
-  # Fractional part
-  frac_bstring = frac2bin(fractional, 127);
-
-  # Constructs IEEE Single Fromat string
-  ieee_string = build_ieee_string(sign_bstring, dec_bstring, frac_bstring);
-endfunction
-
-function ieee_string = build_ieee_string(sign_bstring, dec_bstring, frac_bstring)
+function ieee_string = build_iee_single_representation(dec_bstring, frac_bstring, sign_bstring)
   # Our number N is: |N| < 1 ------> exponent < 0
   if dec_bstring(1) == "0" && length(dec_bstring) == 1
     exponent = first_occurence_of_bit_1(frac_bstring);
@@ -76,7 +73,6 @@ endfunction
 # Locates the first occurence of the digit 1 in a fracional binary string and returns its index
 function occ = first_occurence_of_bit_1(bstring)
   try
-
     occ = strchr(bstring, '1');
     occ = occ(1);
   catch
@@ -141,64 +137,18 @@ function string = frac2bin(fractional, len)
   endwhile
 endfunction
 
-# Converts input string representing a number in base 2 to an integer number
-function value = integer_binary_number(string)
+
+# Validates string that must be representing a number in either base 2 or 10
+function value_string = is_numeric_input(string, is_binary)
   if all(isstrprop(string, "digit"))
-    pass_if_binary(string);
-    value = int64(bin2dec(string));
-  else
-    printf("Invalid format. Was expecting a number.");
-    exit;
-  endif
-endfunction
-
-# Converts input string representing a number in base 10 to an integer number
-function value = integer_number(string)
-  if all(isstrprop(string, "digit"))
-    value = int64(str2num(string));
-  else
-    printf("Invalid format. Was expecting a number.");
-    exit;
-  endif
-endfunction
-
-# Converts input string representing a number in base 2 to a float number
-function value = float_binary_number(dec_string, frac_string)
-  if all(isstrprop(dec_string, "digit")) && all(isstrprop(frac_string, "digit"))
-    pass_if_binary(dec_string); pass_if_binary(frac_string);
-    value = double(bin2dec(dec_string));
-
-    exp = 1;
-    while exp <= length(frac_string)
-      value = value + (double(str2num(frac_string(exp))) * double((2 ** (-1 * exp))));
-      exp++;
-    endwhile
-  else
-    printf("Invalid format. Was expecting a number.");
-    exit;
-  endif
-endfunction
-
-# Converts input string representing a number in base 10 to a float number
-function value = float_number(dec_string, frac_string)
-  if all(isstrprop(dec_string, "digit")) && all(isstrprop(frac_string, "digit"))
-    value = str2double(strcat(dec_string, strcat(".", frac_string)));
-  else
-    printf("Invalid format. Was expecting a number.");
-    exit;
-  endif
-endfunction
-
-# Check whether the string contains only the characters '0' or '1'. If not, terminates execution.
-function pass_if_binary(string)
-  i = 1;
-  while i <= length(string)
-    if string(i) != '0' && string(i) != '1'
-      printf("Invalid format. Was expecting a binary string.");
-      exit;
+    if is_binary
+      pass_if_binary(string);
     endif
-    i++;
-  endwhile
+    value_string = string;
+  else
+    printf("Invalid format. Was expecting a number.");
+    exit;
+  endif
 endfunction
 
 # Strips sign from input string and return an indicator 1 for + or -1 for -.
@@ -214,8 +164,8 @@ function [string, sign] = strip_sign_from_string(string)
   endif
 endfunction
 
-# Converts input string to a number
-function [value, sign] = input_to_number(string)
+# Converts input string to the expected number string
+function [value_bstring, sign] = input_to_number(string)
   # Strips sign from string
   [string, sign] = strip_sign_from_string(string);
 
@@ -227,12 +177,15 @@ function [value, sign] = input_to_number(string)
     occ = strchr(string, '.');
     # Integer number
     if length(occ) == 0
-      value = integer_binary_number(string);
+      value_string = is_numeric_input(string, true);
     # Floating point number. Format accepted is [0-1]+\.[0-1]+
     elseif length(occ) == 1 && occ > 1 && occ < length(string)
-      dec_string = substr(string, 1, occ - 1);
-      frac_string = substr(string, occ + 1);
-      value = float_binary_number(dec_string, frac_string);
+      dec_string = is_numeric_input(substr(string, 1, occ - 1), true);
+      frac_string = is_numeric_input(substr(string, occ + 1), true);
+      # If it reach this point, we can assign string to value without needing to concatenate,
+      # otherwise is the program would be terminated in binary_input_to_bstring. This guarantees
+      # the string is in expected format.
+      value_bstring = string;
     # Error
     else
       printf("Invalid format. Bad use of floating point string detected. Accepted format for float is '[0-1]+\.[0-1]+'.");
@@ -244,18 +197,80 @@ function [value, sign] = input_to_number(string)
     occ = strchr(string, '.');
     # Integer number
     if length(occ) == 0
-      value = integer_number(string);
+      value_string = is_numeric_input(string, false);
+      value_bstring = decimal_integer_string_to_bstring(value_string);
     # Floating point number. Format accepted is [0-9]+\.[0-9]+
     elseif length(occ) == 1 && occ > 1 && occ < length(string)
-      dec_string = substr(string, 1, occ - 1);
-      frac_string = substr(string, occ + 1);
-      value = float_number(dec_string, frac_string);
+      dec_string = is_numeric_input(substr(string, 1, occ - 1), false);
+      frac_string = is_numeric_input(substr(string, occ + 1), false);
+
+      dec_bstring = decimal_integer_string_to_bstring(dec_string);
+      frac_bstring = fractional_integer_string_to_bstring(frac_string);
+      value_bstring = strcat(dec_bstring, strcat(".", frac_bstring));
     # Error
     else
-      printf("Invalid format. Bad use of floating point string detected. Accepted format for float is '[0-1]+\.[0-1]+'.");
+      printf("Invalid format. Bad use of floating point string detected. Accepted format for float is '[0-9]+\.[0-9]+'.");
       exit;
     endif
   endif
+endfunction
+
+# Converts a decimal integer string to a binary string
+function bstring = decimal_integer_string_to_bstring(string)
+  bstring = "";
+  quocient_number = 1;
+
+  while quocient_number > 0
+    digit = 1;
+    dividend_str = quocient_str = "";
+    while digit <= length(string)
+      dividend_str = strcat(dividend_str, string(digit));
+      dividend = str2num(dividend_str);
+      remainder = rem(dividend, 2);
+      quocient_number = floor(double(dividend) / 2);
+      quocient_str = strcat(quocient_str, num2str(quocient_number));
+      dividend_str = num2str(remainder);
+      digit++;
+    endwhile
+    string = quocient_str;
+    quocient_number = str2num(quocient_str);
+    bstring = strcat(num2str(remainder), bstring);
+  endwhile
+endfunction
+
+# Converts a fractional integer string to a binary string
+function bstring = fractional_integer_string_to_bstring(string)
+  count = 0;
+  bstring = "";
+  unfinished = true;
+
+  while unfinished
+    multiplied_str = "";
+    digit = length(string);
+    carry = 0; all_zero = true;
+    while digit > 0
+      bit_result_str = num2str(carry + (2 * str2num(string(digit))));
+      if length(bit_result_str) == 1
+        carry = 0;
+      else
+        carry = 1;
+      endif
+
+      multiplied_str = strcat(bit_result_str(length(bit_result_str)), multiplied_str);
+
+      if bit_result_str(length(bit_result_str)) != "0"
+        all_zero = false;
+      endif
+      digit--;
+    endwhile
+    string = multiplied_str;
+    bstring = strcat(bstring, num2str(carry));
+
+    count++;
+    if all_zero || count == 127
+      unfinished = false;
+    endif
+  endwhile
 endfunction
 
 # Scans user keyboard. If exit command found, terminates program.
@@ -280,6 +295,18 @@ function print_word(ieee_string)
   printf("[  %c  |  ", sign);
   printf("%c  ", exponent); printf("|  ");
   printf("%c  ", significant); printf("]\n");
+endfunction
+
+# Check whether the string contains only the characters '0' or '1'. If not, terminates execution.
+function pass_if_binary(string)
+  i = 1;
+  while i <= length(string)
+    if string(i) != '0' && string(i) != '1'
+      printf("Invalid format. Was expecting a binary string.");
+      exit;
+    endif
+    i++;
+  endwhile
 endfunction
 
 main();
